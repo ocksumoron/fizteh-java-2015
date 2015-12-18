96a37f5a6094c2d299b8d05f5fea23bfcccfb849
Daniil Dmitriev <ocksumoron@yandex.ru>
2015-12-19 00:19:02 +0300

    threads modified

--- ocksumoron/src/main/java/ru/fizteh/fivt/students/ocksumoron/threads/RollCall.java
+++ ocksumoron/src/main/java/ru/fizteh/fivt/students/ocksumoron/threads/RollCall.java
@@ -1,12 +1,8 @@
 package ru.fizteh.fivt.students.ocksumoron.threads;
 
-import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by ocksumoron on 14.12.15.
@@ -15,19 +11,12 @@ public class RollCall {
 
     private static volatile boolean allReady;
     private static boolean finish;
-    //private static CountDownLatch countDownLatch;
     private static volatile Random chance = new Random();
     private static CyclicBarrier beginAnswer;
     private static CyclicBarrier endAnswer;
     private static final int NO_PROBABILITY = 10;
 
     private static class Player extends Thread {
-
-        public static Semaphore semaphore = new Semaphore(0);
-
-        public void setLock() {
-            semaphore.release();
-        }
 
         @Override
         public void run() {
@@ -56,7 +45,6 @@ public class RollCall {
 
     public static void main(String[] args) {
         finish = false;
-
         int n = Integer.valueOf(args[0]);
         beginAnswer = new CyclicBarrier(n + 1);
         endAnswer = new CyclicBarrier(n + 1);
